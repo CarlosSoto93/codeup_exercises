9068583e964b76eb506ae1d205952115b95441fb
Carlos <c.soto93@hotmail.com>
2017-04-12 13:47:14 -0500

    used the for and the .forEach to display names.

--- js/iterating.js
+++ js/iterating.js
@@ -3,16 +3,19 @@
 
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
 
-        var people = ["Ruben", "Carlos", "Francisco", "Rocio"];
+        var names = ["Ruben", "Carlos", "Francisco", "Rocio"];
 
     // TODO: Create a log statement that will log the number of elements in the names array.
 
-        console.log(people.length);
+        console.log(names.length);
 
     // TODO: Create log statements that will print each of the names array elements individually.
 
-        console.log(people[0]);
-        console.log(people[1]);
-        console.log(people[2]);
-        console.log(people[3]);
+        for (var i = 0; i < names.length; ++i) {
+            console.log(names[i]);
+    }
+
+    names.forEach(function (element, index, array) {
+        console.log(index, element);
+    });
 })();
