b2ab3d08c9cab0fb81f21dfd54ec6f823517d645
Carlos <c.soto93@hotmail.com>
2017-04-14 12:20:04 -0500

    circle exercise

--- js/extra_exercises.js
+++ js/extra_exercises.js
@@ -33,4 +33,33 @@
     var hitMiss = ["Strike", "Score", "You can try again."]
 
 
+// (4-14-17)
+    // Object BONUS:
+    //
+    //     Create a pet object with the following...
+    //
+    //     Properties:
+    //
+    // 1) name - string
+    // 2) species - string
+    // 3) age - number
+    // 4) gender - string
+    // 5) clean - boolean
+    // 6) hungry - boolean
+    // 7) sleepy - boolean
+    // 8 ) happy - boolean
+    // 9) petFood - number 10
+    // 10) soap - number 3
+    //
+    // Methods:
+    //
+    //     1) callPet() - call’s out pet name
+    // 2) playWithPet() - makes pet happy and hungry and dirty
+    // 3) feedPet(petFood) - makes pet not hungry and sleepy and reduces petFood by 1
+    // 4) makePetSleep() - makes pet not sleepy, not happy, and hungry, and 1 older
+    // 5) cleanPet(soap) - makes pet not happy and clean and reduces soap by 1
+    //
+    // Have fun with the pet!
+    //     call it, feed it, play with it, make it sleep, clean it
+
 })();
